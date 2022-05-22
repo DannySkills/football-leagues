222c1fd75215eb7a6ccfb48c62396a7a25d19f52
Daniel Bar-on <danielbo@academy-skills.com>
2022-05-22 11:54:04 +0300

    try-catch adjustments

--- src/services/league.service.ts
+++ src/services/league.service.ts
@@ -1,20 +1,32 @@
-import axios, { AxiosResponse } from 'axios';
-import { Team } from '../interfaces/team';
-import {League} from '../interfaces/league';
+import axios, { AxiosResponse } from "axios";
+import { Team } from "../interfaces/team";
+import { League } from "../interfaces/league";
 
 const getFootballLeagues = async (): Promise<League[]> => {
-    const {data}: AxiosResponse<any> = await axios.get('https://www.thesportsdb.com/api/v1/json/2/all_leagues.php')
-    const leagues: League[] = data.leagues.splice(0,5)
+  try {
+    const { data }: AxiosResponse<any> = await axios.get(
+      "https://www.thesportsdb.com/api/v1/json/2/all_leagues.php"
+    );
+    const leagues: League[] = data.leagues.splice(0, 5);
     return leagues;
-}
+  } catch (err) {
+    throw err;
+  }
+};
 
-const getFootballTeams = async (leagueName: string): Promise<Team[]> => {    
-    const {data}: AxiosResponse<any> = await axios.get(`https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=${leagueName}`)
-    const teams: Team[]= data.teams
+const getFootballTeams = async (leagueName: string): Promise<Team[]> => {
+  try {
+    const { data }: AxiosResponse<any> = await axios.get(
+      `https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=${leagueName}`
+    );
+    const teams: Team[] = data.teams;
     return teams;
-}
+  } catch (err) {
+    throw err;
+  }
+};
 
-export const LeagueService  = {
-    getFootballLeagues,
-    getFootballTeams,
-}
+export const LeagueService = {
+  getFootballLeagues,
+  getFootballTeams,
+};
