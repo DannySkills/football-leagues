84d3a1d0ef1a4a350d29b469ecd49bde599ee9d9
Daniel Bar-on <danielbo@academy-skills.com>
2022-05-23 12:32:32 +0300

    Header changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div className='main-header'>
-        <h1>League4U</h1> 
+    <div className="main-header">
+      <h1>Football Leagues</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
