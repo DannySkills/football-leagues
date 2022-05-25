5b885283137d05e5876f5ee577c22161e67a3d6c
Daniel Bar-on <danielbo@academy-skills.com>
2022-05-25 17:13:30 +0300

    cleaning files

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,18 +4,6 @@ const Header = () => {
   return (
     <div className="main-header">
       <h1>Football Leagues</h1>
-      <ul className="clean-list flex">
-        <li>
-          <a href="/" className="clean-link">
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="/" className="clean-link gap">
-            About
-          </a>
-        </li>
-      </ul>
     </div>
   );
 };
